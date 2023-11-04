233fe0191172054a5fe2825571ac78fb602d3892
Sanyam Jain <sanyamjain10168@gmail.com>
2023-11-05 03:34:54 +0530

    added axios

--- src/api/services/weatherService.js
+++ src/api/services/weatherService.js
@@ -1,10 +1,9 @@
-
+import axios from "axios";
 
 const WEATHER_BASE_URL=`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API}&q=`
 
 export const getWeatherData = async (cityName) => {
   const URL=WEATHER_BASE_URL+cityName+"&api=yes";
-  const res= await fetch(URL);
-  const data= await res.json();
-  return data
+  const res= await axios.get(URL);
+  return res.data
 };
